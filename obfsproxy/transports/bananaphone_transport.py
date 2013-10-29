3a655eea04b66f5766699294717970a25a83040d
David Stainton <dstainton415@gmail.com>
2013-10-29 04:25:07 -0700

    Add key exchange. Not currently functional.

--- obfsproxy/transports/bananaphone_transport.py
+++ obfsproxy/transports/bananaphone_transport.py
@@ -6,8 +6,16 @@ from obfsproxy.network.buffer import Buffer
 
 from bananaphone import rh_decoder, rh_encoder
 
+# https://github.com/seanlynch/pynacl
+import nacl
 
 log = logging.get_obfslogger()
+
+
+ST_WAIT_FOR_PUBLIC_KEY = 0
+ST_WAIT_FOR_SECRET_KEY = 1
+ST_ENCRYPTED           = 2
+
 
 class BananaPhoneBuffer(object):
 
@@ -51,11 +59,75 @@ class BananaphoneTransport(BaseTransport):
             self.corpus = '/usr/share/dict/words'
             log.debug("Setting corpus to default: '%s'", self.corpus)
 
+        self.remote_publicKey = None
+        self.secretKey = nacl.randombytes(nacl.crypto_stream_KEYBYTES)
+        self.publicKey, self.privateKey = nacl.crypto_box_keypair()
         self.bananaBuffer = BananaPhoneBuffer(corpusFilename=self.corpus)
 
+    def handshake(self, circuit):
+        if self.we_are_initiator:
+            log.debug("initiating key exchange")
+            log.debug("sending public key")
+            self.state = ST_WAIT_FOR_PUBLIC_KEY
+            circuit.downstream.write(self.publicKey)
+
+    def sendSecretKey(self, circuit):
+        log.debug("sending secret key")
+        nonce      = nacl.randombytes(nacl.crypto_box_NONCEBYTES)
+        ciphertext = nacl.crypto_box(self.secretKey, nonce, self.publicKey, self.privateKey)
+        circuit.upstream.write(ciphertext)
+
+    def receivedSecretKey(self, data):
+        remote_secretKey = data.peek(nacl.crypto_stream_KEYBYTES)
+        if len(remote_secretKey) == nacl.crypto_stream_KEYBYTES:
+            log.debug("received secret key")
+            self.remote_secretKey = remote_secretKey
+            data.drain()
+            return True
+        log.debug("did NOT received secret key")
+        return False        
+
+    def receivedPublicKey(self, data):
+        remote_publicKey = data.peek(nacl.crypto_box_PUBLICKEYBYTES)
+        if len(remote_publicKey) == nacl.crypto_box_PUBLICKEYBYTES:
+            log.debug("received public key")
+            self.remote_publicKey = remote_publicKey
+            data.drain()
+            return True
+        log.debug("did NOT received public key")
+        return False
+
     def receivedDownstream(self, data, circuit):
-        circuit.upstream.write(self.bananaBuffer.transcribeFrom(data.read()))
-        return
+
+        if self.state == ST_ENCRYPTED:
+            circuit.upstream.write(self.bananaBuffer.transcribeFrom(data.read()))
+            return
+
+        if self.state == ST_WAIT_FOR_PUBLIC_KEY:
+            if not self.receivedPublicKey(data):
+                return
+
+            if self.we_are_initiator:
+                self.state = ST_WAIT_FOR_SECRET_KEY
+                self.sendSecretKey(circuit)
+                log.debug("waiting for server's secret key")
+                return
+            else:
+                self.state = ST_WAIT_FOR_SECRET_KEY
+                log.debug("sending public key")
+                circuit.upstream.write(self.publicKey)
+                log.debug("waiting for client's secret key")
+                return
+
+        if self.state == ST_WAIT_FOR_SECRET_KEY:
+            if not self.receivedSecretKey(data):
+                return
+            self.state = ST_ENCRYPTED
+            if self.we_are_initiator:
+                self.sendSecretKey(circuit)
+                log.info("key exchange complete!")
+                return
+
 
     def receivedUpstream(self, data, circuit):
         circuit.downstream.write(self.bananaBuffer.transcribeTo(data.read()))
@@ -74,8 +146,17 @@ class BananaphoneTransport(BaseTransport):
 
 
 class BananaphoneClient(BananaphoneTransport):
-    pass
+    
+    def __init__(self, transport_config):
+        self.we_are_initiator = True
+
+        BananaphoneTransport.__init__(self, transport_config)
 
 class BananaphoneServer(BananaphoneTransport):
-    pass
+   
+    def __init__(self, transport_config):
+        self.we_are_initiator = False
+        self.state            = ST_WAIT_FOR_PUBLIC_KEY
+
+        BananaphoneTransport.__init__(self, transport_config)
 
